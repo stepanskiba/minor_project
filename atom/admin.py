e91808923cb6cc9facb3702c1d3e1695dfdd30b2
stepanskiba <skibastepan@gmail.com>
2023-06-07 05:10:41 +0300

    new

--- atom/admin.py
+++ atom/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import UserModels, TaskModels, SolutionModels, ServiceModels
+from .models import ScheduleTopicModels
 # Register your models here.
+
+admin.site.register(UserModels)
+admin.site.register(TaskModels)
+admin.site.register(SolutionModels)
+admin.site.register(ServiceModels)
+admin.site.register(ScheduleTopicModels)
